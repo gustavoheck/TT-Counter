8e2b54b3bef0e8fb9a4c7261ee221e5f5e5e1dd7
Gustavo Henrique Heck <gustavohenriqueheck@gmail.com>
2026-04-03 22:15:49 -0300

    feat[dices]: adding multilanguage at dices screen

--- src/screens/dices/Details.js
+++ src/screens/dices/Details.js
@@ -1,9 +1,11 @@
 import { Text } from "react-native";
 
 import { styleDetails } from "./styles/styleDetails";
+import { useTranslation } from "react-i18next";
 
 export default function Details() {
+	const { t } = useTranslation();
 	return <>
-		<Text style={styleDetails.title}>Dices</Text>
+		<Text style={styleDetails.title}>{t('mainMenu.dicesPlaceholder')}</Text>
 	</>
 };
